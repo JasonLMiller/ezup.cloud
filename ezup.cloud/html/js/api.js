870df3f887a3fce3ba557210da676b2e6e700fe3
JasonLMiller <jasonmiller@ezup.com>
2021-02-08 12:59:17 -0800

    day 8

--- ezup.cloud/html/js/api.js
+++ ezup.cloud/html/js/api.js
@@ -1,23 +1,29 @@
 //API JS 
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://ezup.design/wp-json/wp/v2/posts?include[]=247'); // using parameter to get a post by ID
+request.open('GET', 'https://ezup.design/wp-json/wp/v2/posts?exclude[]=207'); // using parameter to get a post by ID
 
 request.onload = function () {
   var response = request.response
   var parsedData = JSON.parse(response); //string to an object
   console.log(parsedData);
-  var title = parsedData[0].title.rendered;
-  var content = parsedData[0].content.rendered;
 
-  var postTitle = document.createElement('h3');
-  var postContent = document.createElement('p');
 
-  postTitle.innerHTML = title;
-  postContent.innerHTML = content;
-  document.body.appendChild(postTitle);
-  document.body.appendChild(postContent);
-}
+  for (item in parsedData) {
+
+    var title = parsedData[item].title.rendered;
+    var content = parsedData[item].content.rendered;
+
+    var postTitle = document.createElement('h3');
+    var postContent = document.createElement('p');
+
+    postTitle.innerHTML = title;
+    postContent.innerHTML = content;
+    document.body.appendChild(postTitle);
+    document.body.appendChild(postContent);
+  }
+
+
+};
 
 request.send();
-//END API
